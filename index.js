13640e517c138b7368bd08ab34fef3339fe820bd
Yeohoon <yhoon.yoon@gmail.com>
2022-01-16 23:55:49 -0500

    Completed Assigment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Your code goes here
+
+document.addEventListener("DOMContentLoaded", function() {
+  let originalWord = document.querySelector("p")
+  originalWord.textContent = "This is really cool!"
+  })
+
